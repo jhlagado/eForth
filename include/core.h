9cf07faf0a09f8641ac5e9ab71cc4ce1901e18cd
John Hardy <john.hardy@workjam.com>
2021-01-13 22:45:11 +1100

    reorg

--- include/core.h
+++ include/core.h
@@ -1,3 +1,23 @@
 #pragma once
 
+extern int BRAN, QBRAN, DONXT, DOTQP, STRQP, TOR, ABORQP, IMEDD, COMPO;
+
 void HEADER(int lex, const char seq[]);
+int CODE(int len, ...);
+int COLON(int len, ...);
+int LABEL(int len, ...);
+
+void BEGIN(int len, ...);
+void AGAIN(int len, ...);
+void UNTIL(int len, ...);
+void WHILE(int len, ...);
+void REPEAT(int len, ...);
+void IF(int len, ...);
+void ELSE(int len, ...);
+void THEN(int len, ...);
+void FOR(int len, ...);
+void NEXT(int len, ...);
+void AFT(int len, ...);
+void DOTQ(const char seq[]);
+void STRQ(char seq[]);
+void ABORQ(const char seq[]);
